a34ca477b7de605b772a599599404489d580e15c
Rayzl2 <60479644+Rayzl2@users.noreply.github.com>
2023-08-31 00:11:12 +0300

    Update UpdateCart.cs

--- Carts.Application/Carts/Commands/UpdatingCart/UpdateCart.cs
+++ Carts.Application/Carts/Commands/UpdatingCart/UpdateCart.cs
@@ -1,8 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using MediatR;
 
 namespace Carts.Application.Carts.Commands.UpdatingCart
