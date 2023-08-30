32faca0469903bfe7367a593ea5616170caf70e5
Rayzl2 <60479644+Rayzl2@users.noreply.github.com>
2023-08-30 23:55:42 +0300

    Update DBInit.cs

--- Carts.Persistence/DBInit.cs
+++ Carts.Persistence/DBInit.cs
@@ -1,18 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace Carts.Persistence
 {
     public class DBInit
     {
+
+        // ПОДКЛЮЧЕНИЕ К ТАБЛИЦЕ CARTS
         public static void InitCartsDbContext(CartsDbContext context)
         {
             context.Database.EnsureCreated();
         }
 
+        // ПОДКЛЮЧЕНИЕ К ТАБЛИЦЕ GOODS
         public static void InitGoodsDbContext(GoodsDbContext context)
         {
             context.Database.EnsureCreated();
