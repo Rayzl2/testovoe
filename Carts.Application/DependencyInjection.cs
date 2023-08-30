abcd2d69082ffa5700cc4d604a60c24abdd0e6a7
Rayzl2 <60479644+Rayzl2@users.noreply.github.com>
2023-08-31 00:01:53 +0300

    Update DependencyInjection.cs

--- Carts.Application/DependencyInjection.cs
+++ Carts.Application/DependencyInjection.cs
@@ -8,10 +8,13 @@ namespace Carts.Application
 {
     public static class DependencyInjection
     {
+
         public static IServiceCollection AddApplication(this IServiceCollection services) {
             var assembly = Assembly.GetExecutingAssembly();
+            // ОПРЕДЕЛЕНИЕ ВСЕХ МЕТОДОВ ВАЗИМОДЕЙСТВИЯ 
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(assembly));
 
+            // ДОБАВЛЕНИЕ ВАЛИДАТОРОВ ДАННЫХ ДЛЯ ПРОВЕРКИ ПОСТУПАЮЩИЙ ДАННЫХ
             services.AddValidatorsFromAssemblies(new[] { assembly });
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
 
