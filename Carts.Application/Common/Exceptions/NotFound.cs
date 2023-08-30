7d6056de4ec80b2ca09f681e9a56e5d43f4a6547
Rayzl2 <60479644+Rayzl2@users.noreply.github.com>
2023-08-31 00:04:24 +0300

    Update NotFound.cs

--- Carts.Application/Common/Exceptions/NotFound.cs
+++ Carts.Application/Common/Exceptions/NotFound.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,7 +8,7 @@ namespace Carts.Application.Common.Exceptions
 {
     public class NotFound : Exception
     {
-
+        // ВОЗВРАЩЕНИЕ НЕНАЙДЕННОЙ СВЯЗКИ НА ГЛОБАЛЬНОМ УРОВНЕ ПРИЛОЖЕНИЯ
         public NotFound(string name, object key) : base($"Entity {name}: {key} not found!") { }
     }
 }
