05aaf3ca21ba7360ccf4d4e2d59c2f306c18034a
Rayzl2 <60479644+Rayzl2@users.noreply.github.com>
2023-08-31 00:10:01 +0300

    Update DeleteCart.cs

--- Carts.Application/Carts/Commands/DeletingCart/DeleteCart.cs
+++ Carts.Application/Carts/Commands/DeletingCart/DeleteCart.cs
@@ -1,9 +1,4 @@
-﻿using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using MediatR;
 
 namespace Carts.Application.Carts.Commands.DeletingCart
 {
