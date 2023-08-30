551b65e38d9bcf5d6a27d218d2edc6d208ce62a8
Rayzl2 <60479644+Rayzl2@users.noreply.github.com>
2023-08-30 23:50:26 +0300

    Update BaseController.cs

--- Carts.WebApi/Controllers/BaseController.cs
+++ Carts.WebApi/Controllers/BaseController.cs
@@ -1,7 +1,5 @@
-﻿using MediatR;
+using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using System.Security.Claims;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace Carts.WebApi.Controllers
 {
@@ -14,13 +12,5 @@ namespace Carts.WebApi.Controllers
         protected IMediator Mediator =>
             _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
 
-        internal Guid machineId => !User.Identity.IsAuthenticated ? Guid.Empty : 
-            Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
-
-        internal Guid SessionId => !User.Identity.IsAuthenticated ? Guid.Empty :
-            Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
-
-        internal Guid Goods => !User.Identity.IsAuthenticated ? Guid.Empty :
-            Guid.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value);
     }
 }
